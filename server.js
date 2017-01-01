ac91192b16c2c003db0644fcce92d82e1ad40733
Daniel Varela <dotexe0@gmail.com>
2017-01-01 11:13:26 -0800

    fixed test-server

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.send('Hello world');
+})
 app.listen(process.env.PORT || 8080);
 console.log('Server running @ localhost:8080');
+
+exports.app = app;
