d0ba7d9a8b929dcbf223fdb208ccf662d1bcf008
Daniel Varela <dotexe0@gmail.com>
2017-01-02 01:12:43 -0800

    added passport functionality with salted passwords

---
+++ models/user-model.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+UserSchema.methods.validatePassword = function(password, callback) {
+    bcrypt.compare(password, this.password, function(err, isValid) {
+        if (err) {
+            callback(err);
+            return;
+        }
+        callback(null, isValid);
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,11 +1,175 @@
+"use strict";
 var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var config = require('./config');
+
 var app = express();
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
+var runServer = function(callback) {
+  mongoose.createConnection(config.DATABASE_URL, function(err) {
+      if (err && callback) {
+          return callback(err);
+      }
+
+      app.listen(config.PORT, function() {
+          console.log('Listening on localhost:' + config.PORT);
+          if (callback) {
+              callback();
+          }
+      });
+  });
+};
+
+if (require.main === module) {
+  runServer(function(err) {
+      if (err) {
+          console.error(err);
+      }
+  });
+};
+
+// PASSPORT AUTH
+
+//////
+var passport = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+
+var strategy = new BasicStrategy(function(username, password, callback) {
+    User.findOne({
+        username: username
+    }, function (err, user) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        if (!user) {
+            return callback(null, false, {
+                message: 'Incorrect username.'
+            });
+        }
+
+        user.validatePassword(password, function(err, isValid) {
+            if (err) {
+                return callback(err);
+            }
+
+            if (!isValid) {
+                return callback(null, false, {
+                    message: 'Incorrect password.'
+                });
+            }
+            return callback(null, user);
+        });
+    });
+});
+
+passport.use(strategy);
+
+/////////////////
+
+var User = require('./models/user-model');
+var bcrypt = require('bcryptjs');
+
+var jsonParser = bodyParser.json();
+
+app.post('/users', jsonParser, function(req, res) {
+    if (!req.body) {
+        return res.status(400).json({
+            message: "No request body"
+        });
+    }
+
+    if (!('username' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: username'
+        });
+    }
+
+    var username = req.body.username;
+
+    if (typeof username !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: username'
+        });
+    }
+
+    username = username.trim();
+
+    if (username === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: username'
+        });
+    }
+
+    if (!('password' in req.body)) {
+        return res.status(422).json({
+            message: 'Missing field: password'
+        });
+    }
+
+    var password = req.body.password;
+
+    if (typeof password !== 'string') {
+        return res.status(422).json({
+            message: 'Incorrect field type: password'
+        });
+    }
+
+    password = password.trim();
+
+    if (password === '') {
+        return res.status(422).json({
+            message: 'Incorrect field length: password'
+        });
+    }
+
+    //bcrypt
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+
+        bcrypt.hash(password, salt, function(err, hash) {
+            if (err) {
+                return res.status(500).json({
+                    message: 'Internal server error'
+                });
+            }
+
+            var user = new User({
+                username: username,
+                password: hash
+            });
+
+            user.save(function(err) {
+                if (err) {
+                    return res.status(500).json({
+                        message: 'Internal server error'
+                    });
+                }
+
+                return res.status(201).json({});
+            });
+        });
+    });
+  });
+
+
+mongoose.connect('mongodb://localhost/auth').then(function() {
+    // app.listen(process.env.PORT || 8080);
+});
 
 app.get('/', function(req, res) {
   res.send('Hello world');
 })
-app.listen(process.env.PORT || 8080);
-console.log('Server running @ localhost:8080');
 
 exports.app = app;
+exports.runServer = runServer;
