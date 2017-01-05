524a79db4f6134dd3d902297201adb036389f3f2
Daniel Varela <dotexe0@gmail.com>
2017-01-04 19:24:09 -0800

    added dashboard endpoint and login signup

--- public/index.js
+++ public/index.js
@@ -17,40 +17,47 @@ $(document).ready(function (e) {
   });
 
   //sign up a new user
-  $('.signup-form').submit(function() {
-    console.log("DID THIS WORK");
-    var usernameVal = $('#email').val();
+  $('.signup-form').submit(function(e) {
+    e.preventDefault();
+    console.log("DID user WORK");
+    var usernameVal = $('#username').val();
     var passwordVal = $('#password').val();
     var user = {
       username: usernameVal,
       password: passwordVal
     };
+    console.log("created ", user);
+    console.log(JSON.stringify(user));
+
     var ajax = $.ajax('/users', {
         type: 'POST',
         data: JSON.stringify(user),
         dataType: 'json',
         contentType: 'application/json'
+    }).done(function() {
+      console.log("Sign me up and log ME IN PLEASE");
+      window.location.href="/dashboard";
     });
-    console.log("LOG ME IN PLEASE");
-    ajax.done();
   });
 
   //login using existing account
-  $('.login-form').submit(function() {
-    console.log("DID THIS WORK");
-    var usernameVal = $('#email').val();
-    var passwordVal = $('#password').val();
+  $('.login-form').submit(function(e) {
+    e.preventDefault();
+    console.log("DID login WORK");
+    var usernameVal = $('#usernameLogin').val();
+    var passwordVal = $('#passwordLogin').val();
     var user = {
       username: usernameVal,
       password: passwordVal
     };
-    var ajax = $.ajax('/dashboard', {
-        type: 'GET',
+    var ajax = $.ajax('/login', {
+        type: 'POST',
         data: JSON.stringify(user),
         dataType: 'json',
         contentType: 'application/json'
+    }).done(function() {
+      console.log("LOG ME IN PLEASE");
+      window.location.href="/dashboard";
     });
-    console.log("LOG ME IN PLEASE");
-    ajax.done();
   });
 });
