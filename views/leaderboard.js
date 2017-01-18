ca8895cc8189f9d58956c3366bf8e49e1f7028b4
Daniel Varela <dvarela@losaltosca.gov>
2017-01-18 13:18:15 -0800

    added styling to index.html and leaderboard.ejs

--- views/leaderboard.js
+++ views/leaderboard.js
@@ -7,41 +7,45 @@ $(document).ready(() => {
 
   let users = window.users;
 
-
+  //set user object's worth = to true worth including funds.
   for (let i = 0; i < users.length; i++) {
     users[i].local.portfolio.worth = round((users[i].local.portfolio.worth + users[i].local.portfolio.funds), 2).toFixed(2);
   }
 
+  let ranking = new Array();
+  for (let i = 0; i < users.length; i++) {
+    ranking.push({'username' : users[i].local.email, 'worth' : users[i].local.portfolio.worth});
+  }
+  // console.log("rank? ", ranking);
+  // EXTRACT USER WORTH AND MAKE AN ARRAY OF OBJECTS WITH KEY "USER" VALUE "WORTH", THEN SORT.
+  var sort_by = function(field, reverse, primer){
 
-  EXTRACT USER WORTH AND MAKE AN ARRAY OF OBJECTS WITH KEY "USER" VALUE "WORTH", THEN SORT.
-//   var sort_by = function(field, reverse, primer){
-//
-//      var key = primer ?
-//          function(x) {return primer(x[field])} :
-//          function(x) {return x[field]};
-//
-//      reverse = !reverse ? 1 : -1;
-//
-//      return function (a, b) {
-//          return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
-//        }
-//   }
-//
-//   users.sort(sort_by('local.portfolio.worth', true, parseInt));
-// console.log(users);
+     var key = primer ?
+         function(x) {return primer(x[field])} :
+         function(x) {return x[field]};
+
+     reverse = !reverse ? 1 : -1;
+
+     return function (a, b) {
+         return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+       }
+  }
+
+  ranking.sort(sort_by('worth', true, parseInt));
+// console.log("sorted?", ranking);
 
 
 
   // var worth;
-  for (let i = 0; i < users.length; i++) {
+  for (let i = 0; i < ranking.length; i++) {
     // worth = users[i].local.portfolio.worth;
-    $('.rank').append('<li>' + '<h6 class="username">' + users[i].local.email + ': <h6 class="number">$' + users[i].local.portfolio.worth + '</h6></li>');
+    $('.rank').append('<li class="list-group-item list-group-item-action list-group-item-success">' + '<h6 class="username">' + ranking[i].username + ': <h6 class="number">$' + ranking[i].worth + '</h6></li>');
     // worth = 0;
   }
 
-  var options = {
-    valueNames: ['number']
-};
-var myList = new List('my-list', options);
-console.log(myList);
+//   var options = {
+//     valueNames: ['number']
+// };
+// var myList = new List('my-list', options);
+// console.log(myList);
 });
