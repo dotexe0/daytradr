ecf295ad10f0eeec7ca8f4c920c8f8fac4338567
Daniel Varela <dvarela@losaltosca.gov>
2017-02-02 15:10:43 -0800

    fixed leaderboard not updating automatically

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 module.exports = {
-   'url' : 'mongodb://admin:password@ds151068.mlab.com:51068/daytradr'
 
-  // 'url' : 'mongodb://localhost/daytradr'
+  //  'url' : 'mongodb://admin:password@ds151068.mlab.com:51068/daytradr'
+
+  'url' : 'mongodb://localhost/daytradr'
 };
 
 // exports.DATABASE_URL = process.env.DATABASE_URL ||
--- views/leaderboard.js
+++ views/leaderboard.js
@@ -6,6 +6,20 @@ $(document).ready(() => {
   }
 
   let users = window.users;
+  // update user worth based on current market value
+  for (let i = 0; i < users.length; i++) {
+    for (let key in users[i].local.portfolio.stocks) {
+      if (users[i].local.portfolio.stocks.hasOwnProperty(key) && key !== "") {
+        const robinhoodURL = 'https://api.robinhood.com/quotes/?symbols='
+        $.getJSON(robinhoodURL + key , (data) => {
+        })
+        .done((data) => {
+            stockAskPrice = data.results[0].bid_price;
+            users[i].local.portfolio.worth += (stockAskPrice * users[i].local.portfolio.stocks[key]);
+        });
+      }
+    }
+  }
 
   //set user object's worth = to true worth including funds.
   for (let i = 0; i < users.length; i++) {
@@ -16,7 +30,6 @@ $(document).ready(() => {
   for (let i = 0; i < users.length; i++) {
     ranking.push({'username' : users[i].local.email, 'worth' : users[i].local.portfolio.worth});
   }
-  // console.log("rank? ", ranking);
   // EXTRACT USER WORTH AND MAKE AN ARRAY OF OBJECTS WITH KEY "USER" VALUE "WORTH", THEN SORT.
   var sort_by = function(field, reverse, primer){
 
@@ -32,20 +45,10 @@ $(document).ready(() => {
   }
 
   ranking.sort(sort_by('worth', true, parseInt));
-// console.log("sorted?", ranking);
 
 
-
-  // var worth;
   for (let i = 0; i < ranking.length; i++) {
-    // worth = users[i].local.portfolio.worth;
     $('.rank').append('<li class="list-group-item list-group-item-action list-group-item-success">' + '<h6 class="username">' + ranking[i].username + ': <h6 class="number">$' + ranking[i].worth + '</h6></li>');
-    // worth = 0;
   }
 
-//   var options = {
-//     valueNames: ['number']
-// };
-// var myList = new List('my-list', options);
-// console.log(myList);
 });
